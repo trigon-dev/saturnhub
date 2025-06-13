6373fb227c7db7db66385fd1667d8654e4044c30
Trigon Dev <ultimate.beat.masher@gmail.com>
2025-06-13 14:52:39 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -5,25 +5,26 @@ local Window = Luna:CreateWindow({
 	Subtitle = nil,
 	LogoID = "86704560187347",
 	LoadingEnabled = true,
-	LoadingTitle = "Saturn Loader",
+	LoadingTitle = "Saturn Hub Loader",
 	LoadingSubtitle = "by coolio",
 
 	ConfigSettings = {
 		RootFolder = nil,
 		ConfigFolder = "SaturnHub"
 	},
-
-	KeySystem = false,
+	
+	KeySystem = true, -- As Of Beta 6, Luna Has officially Implemented A Key System!
 	KeySettings = {
 		Title = "Saturn Hub",
 		Subtitle = "Key System",
-		Note = "",
-		SaveInRoot = false,
-		SaveKey = true,
-		Key = {"Example Key"},
+		Note = "Join the Discord server to get the key.",
+		SaveInRoot = false, -- Enabling will save the key in your RootFolder (YOU MUST HAVE ONE BEFORE ENABLING THIS OPTION)
+		SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
+		Key = {"Example Key"}, -- List of keys that will be accepted by the system, please use a system like Pelican or Luarmor that provide key strings based on your HWID since putting a simple string is very easy to bypass
 		SecondAction = {
-			Enabled = true,
-			Type = "Link",
-			Parameter = ""
+			Enabled = true, -- Set to false if you do not want a second action,
+			Type = "Link", -- Link / Discord.
+			Parameter = "https://discord.com/invite/TyevewM7Jc" -- If Type is Discord, then put your invite link (DO NOT PUT DISCORD.GG/). Else, put the full link of your key system here.
+		}
 	}
 })
